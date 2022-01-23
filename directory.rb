c0cc84ef63e13bb84ef9a58438443e4ef81a3007
meghanblyth <megblyth@gmail.com>
2022-01-23 21:16:28 +0000

    Refactoring the code

--- directory.rb
+++ directory.rb
@@ -1,50 +1,54 @@
-# Adding an interactive menu
-def interactive_menu
-  students = []
-  loop do
-    # 1. print the menu and ask the user what to do
-
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-
-    # 2. Read the input and save it into a variable
-    selection = gets.chomp
-
-    # 3. Do what the user has asked
-    case selection
-    when "1"
-      students = input_students
-    when "2"
-      print_header
-      print(students)
-      print_footer(students)
-    when "9"
-      exit # this will cause the program to terminate
-    else
-      puts "I don't know what you meant, try again"
-    end
-  end
-end
+# Creating an empty array accessible to all methods
+@students = []
 
 # let's allow the user to input students into an array
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, jist hit return twice"
-  # create an empty array
-  students = []
   # get the first name
   name = gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
-    students << { name: name, cohort: :november }
-    puts "Now we have #{students.count} students"
+    @students << { name: name, cohort: :november }
+    puts "Now we have #{@students.count} students"
     # get another name from the user
     name = gets.chomp
   end
-  # return the array of students
-  students
+end
+
+# Adding an interactive menu
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+# Print the menu
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit" # Adding more items later
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit # this will cause the program to terminate
+  else
+    puts "I don't know what you meant, try again"
+  end
 end
 
 # defining our other methods
@@ -53,14 +57,14 @@ def print_header
   puts "-------------"
 end
 
-def print(students)
-  students.each_with_index do |students, index|
-    puts "#{index}. #{students[:name]} (#{students[:cohort]} cohort)"
+def print_student_list
+  @students.each_with_index do |student, index|
+    puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
 
-interactive_menu 
+interactive_menu # Calling the array defined at the beginning
