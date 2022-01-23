b1124334b6246780fc437e5fe4c73b7f3448f092
meghanblyth <megblyth@gmail.com>
2022-01-23 20:42:14 +0000

    Adding an interactive menu

--- directory.rb
+++ directory.rb
@@ -1,3 +1,32 @@
+# Adding an interactive menu
+def interactive_menu
+  students = []
+  loop do
+    # 1. print the menu and ask the user what to do
+
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+
+    # 2. Read the input and save it into a variable
+    selection = gets.chomp
+
+    # 3. Do what the user has asked
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit # this will cause the program to terminate
+    else
+      puts "I don't know what you meant, try again"
+    end
+  end
+end
+
 # let's allow the user to input students into an array
 def input_students
   puts "Please enter the names of the students"
@@ -25,16 +54,13 @@ def print_header
 end
 
 def print(students)
-  students.each do |students|
-    puts "#{students[:name]} (#{students[:cohort]} cohort)"
+  students.each_with_index do |students, index|
+    puts "#{index}. #{students[:name]} (#{students[:cohort]} cohort)"
   end
 end
 
 def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
-# printing to the user
-students = input_students
-print_header
-print(students)
-print_footer(students)
+
+interactive_menu 
